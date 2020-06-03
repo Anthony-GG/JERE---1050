baf2c8abee8a1595c3ac0f354e37a089eb2a64b7
Anthony Iacano <iacano5@outlook.com>
2020-06-03 16:28:37 -0400

    I hate myself.

---
+++ Worksheet 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Worksheet_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //Part 1
+            
+            int taquitoCount = 15;
+
+            Console.WriteLine("How many Empanadas are there?");
+            int empanadaCount = Convert.ToInt32(Console.ReadLine());
+
+            bool checkFood(int taquito, int empanada)
+            {
+                if (taquitoCount > 0 && empanada > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            checkFood(taquitoCount, empanadaCount);
+
+
+            //Part 2
+            void eatPlate()
+            {
+             if(checkFood(taquitoCount, empanadaCount))
+                {
+                    taquitoCount--;
+                    empanadaCount--;
+
+                    Console.WriteLine("Taquitos Left: " + taquitoCount);
+                    Console.WriteLine("Empanadas Left: " + empanadaCount);
+                }    
+            }
+
+            eatPlate();
+
+
+        }
+    }
+}
