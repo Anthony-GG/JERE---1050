0ef57230910a2483519eb9d336829ff00441d161
Anthony Iacano <iacano5@outlook.com>
2020-07-08 14:02:56 -0400

    -Added Worksheet 4

---
+++ Worksheet 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Worksheet_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Part 1 - :sunglasses:
+
+            //List<string> depression = new List<string>();
+
+            //Taquito tacky = new Taquito(274, depression);
+
+            //tacky.addTopping("beans");
+
+            //tacky.addTopping("apples");
+
+            //tacky.addTopping("blood of the innocent");
+
+            //tacky.addTopping("sleep");
+
+            //tacky.printToppings();
+
+            ////Part 2 - Clalories
+            //tacky.GetCalories();
+
+            ////Part 3 - Loop-de-loops
+
+            List<Taquito> sadness = new List<Taquito>();
+            for (int i = 0; i > 10; i++)
+            {
+                List<string> argh = new List<string>();
+                Taquito tacky2 = new Taquito(i * 10, argh);
+                sadness.Add(tacky2);
+            }
+            
+
+
+        }
+    }
+}
+
+
---
+++ Worksheet 4/Taquito.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Worksheet_4
+{
+    class Taquito
+    {
+        public List<string> toppings = new List<string>();
+        private int calories;
+
+        public Taquito(int Calories, List<string> Toppings)
+        {
+            calories = Calories;
+            toppings = Toppings;
+        }
+
+        public void addTopping(string toppingType)
+        {
+           toppings.Add(toppingType);
+        }
+
+
+        public void printToppings()
+        {
+            foreach (string food in toppings)
+            {
+                Console.WriteLine(food);
+            }
+            Console.WriteLine(" ");
+        }
+
+
+        public int GetCalories()
+        {
+            return calories;   
+        }
+
+
+
+    }
+}
