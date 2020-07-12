939fca9b1027e4dc3de73db0f7ec66f8643ef84b
Anthony Iacano <iacano5@outlook.com>
2020-07-12 16:28:18 -0400

    -Added Worksheet 5, 1/4

---
+++ Worksheet 5/Food.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worksheet_5
+{
+    public abstract class Food
+    {
+
+        public List<string> toppings { get; set; }
+        public int calories { get; set; }
+
+        protected Food(int value)
+        {
+
+            calories = value;
+
+        }
+
+        public void addTopping(string toppingValue)
+        {
+            toppings.Add(toppingValue);
+        }
+
+        public override string ToString()
+        {
+            return "Make it say whatever idc";
+        }
+
+        public abstract int FoodRating();
+
+
+    }
+}
